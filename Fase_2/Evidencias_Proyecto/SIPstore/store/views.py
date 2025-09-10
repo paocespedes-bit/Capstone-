88fa8993b5293bb8e26bfbac4d7b63e29fc77e1e
Cristian Nunez <tu_correo@example.com>
2025-09-10 15:05:44 -0300

    Subiendo proyecto Django

---
+++ Fase_2/Evidencias_Proyecto/SIPstore/store/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+#! Aqui se agregan las views (templates).
+
+def index(request):
+    return render(request, 'index.html')
+
+
+
+
