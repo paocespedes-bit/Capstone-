93736099b427f31e40fb8b5990bb57ac2f4476ce
Cristian Nunez <tu_correo@example.com>
2025-09-28 11:16:08 -0300

    funcion filtro

--- Fase_2/Evidencias_Proyecto/SIPstore/store/views.py
+++ Fase_2/Evidencias_Proyecto/SIPstore/store/views.py
@@ -5,18 +5,15 @@ from .models import PanelSIP, KitConstruccion
 #! Aqui se agregan las views (templates).
 def paneles(request):
  # Filtros para paneles
-    search = request.GET.get('search', '').strip()
-
     tipo_obs_filtro = request.GET.get('tipo_obs', '').strip()
     Espesor_OSB_filtro = request.GET.get('espesor', '').strip()
     madera_union_filtro = request.GET.get('madera_union', '').strip()
-
-    precio_max = request.GET.get('precio_max', '').strip()
+    precios = PanelSIP.objects.aggregate(precio_min=Min('precio'), precio_max=Max('precio'))
+    
+    precio_min = precios['precio_min']
+    precio_max = precios['precio_max']
 
     paneles_sip = PanelSIP.objects.all()
-    # Filtros para paneles por busqueda de caracteres
-    if search:
-        paneles_sip = paneles_sip.filter(tipo_obs__icontains=search)
     # Filtros para paneles por select opcional de tipo de filtro
     if tipo_obs_filtro:
         paneles_sip = paneles_sip.filter(tipo_obs=tipo_obs_filtro)
@@ -27,7 +24,7 @@ def paneles(request):
     if madera_union_filtro:
         paneles_sip = paneles_sip.filter(madera_union=madera_union_filtro)
     # Filtros para paneles por precio
-    MIN_PRECIO = 0
+    MIN_PRECIO = precio_min 
     paneles_sip = paneles_sip.filter(precio__gte=MIN_PRECIO)
 
     if precio_max:
@@ -45,7 +42,6 @@ def paneles(request):
         'tipos_obs': tipos_obs,
         'espesores': espesores,
         'maderas': maderas,
-        'search': search,
         'tipo_obs_filtro': tipo_obs_filtro,
         'Espesor_OSB_filtro': Espesor_OSB_filtro,
         'madera_union_filtro': madera_union_filtro,
