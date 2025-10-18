638392bedca3503114b944f86efc6dd22fee32a2
CristianNT <cr.nunezt@duocuc.cl>
2025-10-18 15:36:36 -0300

    correccion de errores

--- Fase_2/Evidencias_Proyecto/SIPstore/cart/context_processors.py
+++ Fase_2/Evidencias_Proyecto/SIPstore/cart/context_processors.py
@@ -1,9 +1,23 @@
 def totales_carrito(request):
+    # Obtiene el carrito de la sesión. Si no existe o no es un diccionario, usa un diccionario vacío {}.
+    carrito = request.session.get("carrito", {})
+    if not isinstance(carrito, dict):
+        carrito = {}
+        
     total = 0
-    cantidad_total = 0
-    if "carrito" in request.session.keys():
-        for key, value in request.session["carrito"].items():
-            total += value.get("acumulado", 0) 
-            cantidad_total += value.get("cantidad", 0)
-            
-    return {"total_carrito": total, "cantidad_total": cantidad_total}
+    cantidad_articulos = 0
+    
+    # Ahora puedes llamar .items() de forma segura, ya que 'carrito' es garantizado un diccionario.
+    for key, value in carrito.items():
+        # Asume que 'value' es un diccionario que contiene 'precio' y 'cantidad'
+        try:
+            total += int(value["precio"]) * value["cantidad"]
+            cantidad_articulos += value["cantidad"]
+        except (TypeError, ValueError, KeyError):
+            # Manejo de errores si los datos dentro del carrito no son correctos
+            continue 
+
+    return {
+        "total_carrito": total,
+        "cantidad_articulos_carrito": cantidad_articulos
+    }
