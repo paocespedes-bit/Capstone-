9f014a32a92f5209ca8b12cf73f2664dcea40b53
CarpinchoTactic <danieleltrolaso@gmail.com>
2025-10-27 19:43:28 -0300

    integracion de TransBank

--- Fase_2/Evidencias_Proyecto/SIPstore/cart/urls.py
+++ Fase_2/Evidencias_Proyecto/SIPstore/cart/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('agregar/', views.agregar_producto, name="add_ajax"),
     path('modificar/<str:accion>/', views.modificar_carrito, name="modificar_ajax" ),
     path('crear-pedido/', views.crear_pedido, name='crear_pedido'),
+    path('pago/<int:pedido_id>/', views.iniciar_pago, name='iniciar_pago'),
+    path('pago_exitoso/', views.pago_exitoso, name='pago_exitoso'),
     ]
