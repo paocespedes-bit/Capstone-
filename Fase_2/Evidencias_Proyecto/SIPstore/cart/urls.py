5d48dfb76e73082966675fbfc71d09f492a78acc
Juan Cardenas Alvarez <ju.cardenasa@duocuc.cl>
2025-10-28 20:55:15 -0300

    actualizacion codigo

--- Fase_2/Evidencias_Proyecto/SIPstore/cart/urls.py
+++ Fase_2/Evidencias_Proyecto/SIPstore/cart/urls.py
@@ -11,8 +11,6 @@ urlpatterns = [
     path('pago_exitoso/', views.pago_exitoso, name='pago_exitoso'),
     path('pago_fallido/', views.pago_fallido, name='pago_fallido'),
     path('pago_pendiente/', views.pago_pendiente, name='pago_pendiente'),
-    # path('confirmacion-pago/<int:pedido_id>/', views.confirm_pago, name='confirm_pago'),
-    # path('info-pedido/', views.info_pedido, name='info_pedido'),
-    # path('confirmar-pago-manual/<int:pedido_id>/', views.confirmar_pago_manual, name='confirmar_pago_manual'),
+    
 
     ]
